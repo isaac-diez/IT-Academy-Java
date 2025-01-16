a147ccce523e8f3ff2271ad134e8c9b877d8f692
isaac.diez <isaac.diez@proton.me>
2025-01-16 13:59:53 +0100

    feat: read contents in countries.txt and listed out in the console.

---
+++ 1.3.3-Java-Collections-Nivell1/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(Paths.get("countries.txt"))){
+
+            while(scanner.hasNextLine()){
+
+                String row = scanner.nextLine();
+
+                System.out.println(row);
+            }
+
+
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
